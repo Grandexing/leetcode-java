52b7f90e3c7612dcda25899900106d98f8bf9cc8
grande-xing <1172113701@qq.com>
2017-05-23 20:16:26 +0800

    finish Shortest Unsorted Continuous Subarray

--- src/ShortestUnsortedContinuousSubarray.java
+++ src/ShortestUnsortedContinuousSubarray.java
@@ -1,27 +1,30 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 /**
  * Created by grande on 2017/5/22.
  */
 public class ShortestUnsortedContinuousSubarray {
     public static int findUnsortedSubarray(int[] nums) {
         int length = nums.length;
-        int min = nums[0];
-        int minPos = 0;
-        int max = nums[0];
-        int maxPos = 0;
-        int index = 0;
+        int[] sorted = Arrays.copyOf(nums,length);
+        Arrays.sort(sorted);
         int start = 0;
-        int end = 0;
-        while (index < length){
-            if (nums[index] <= max){
-                start = index - 1;
-                if (nums[index] < min) min = nums[index];
-            } else max = nums[index];
+        int end = length-1;
+        while (start<length){
+           if (nums[start]!= sorted[start]) break;
+           start++;
         }
-        return min;
+        while (end>-1){
+            if (nums[end] != sorted[end]) break;
+            end--;
+        }
+        return end-start>0?end-start+1:0;
     }
 
     public static void main(String[] args) {
-        int[] nums = {2, 6, 2, 4, 8, 10, 9, 15};
+        int[] nums = {2, 6, 4, 8, 10, 9, 15};
+//        int[] nums = {3,1,2,4,5};
         System.out.println(findUnsortedSubarray(nums));
     }
 }
