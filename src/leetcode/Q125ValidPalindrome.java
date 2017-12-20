cb3083f506d0e4e3a715958aaeb113f5ad2fbbd3
grande-xing <1172113701@qq.com>
2017-12-20 15:11:51 +0800

    valid palindrome o(n)

--- src/leetcode/Q125ValidPalindrome.java
+++ src/leetcode/Q125ValidPalindrome.java
@@ -6,17 +6,21 @@ public class Q125ValidPalindrome {
         String s2 = "race a car";
         System.out.println(isPalindrome(s1));
         System.out.println(isPalindrome(s2));
+        System.out.println(isPalindrome("0P"));
     }
     public static boolean isPalindrome(String s) {
+        if (s.isEmpty()) return true;
         char[] cs = s.toCharArray();
         //A-Z 65-90
         //a-z 97-122
         int i = 0;
-        int j = cs.length - i;
+        int j = cs.length - 1;
         i = findNextAlpha(cs, i);
         j = findProvAlpha(cs, j);
-        while (i < j) {
+        while (i <= j) {
         	if (cs[i] != cs[j]) return false;
+        	i++;
+        	j--;
 	        i = findNextAlpha(cs, i);
             j = findProvAlpha(cs, j);
         }
@@ -32,21 +36,25 @@ public class Q125ValidPalindrome {
         		return i;
         	} else if (cs[i] > 96 && cs[i] < 123) {
         		return i;
-        	}
+        	} else if (cs[i] > 47 && cs[i] < 58) {
+        	    return i;
+            }
         }
         return cs.length;    
     }
 
 
     static int findProvAlpha(char[] cs, int j){
-        for (; j<cs.length; j++) {
+        for (; j > 0; j--) {
         	if (cs[j] > 64 && cs[j] < 91) {
         		//cs[i] is upper case, convert to lower case
         		cs[j] = (char)(cs[j] + 32);
         		return j;
         	} else if (cs[j] > 96 && cs[j] < 123) {
         		return j;
-        	}
+        	} else if (cs[j] > 47 && cs[j] < 58) {
+                return j;
+            }
         }
         return 0;
     }
