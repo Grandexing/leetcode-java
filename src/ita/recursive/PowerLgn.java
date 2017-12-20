f8ae59b86d33647e93de3b1edddcf499c1a7135d
邢志壮 <1172113701@qq.com>
2017-12-20 23:15:11 +0800

    lgn power

---
+++ src/ita/recursive/PowerLgn.java
@@ -0,0 +1,22 @@
+package ita.recursive;
+
+/**
+ * Created by grande on 2017/12/20.
+ */
+public class PowerLgn {
+//    复杂度为logn的，递归式的乘方算法
+    public static void main(String[] args) {
+        System.out.println(power(2, 3));
+    }
+
+
+    static int power(int i, int n){
+        if (n < 0) return 0;
+        if (n == 0) return 1;
+        if (n % 2 == 0){
+            return power(i, n/2) * power(i, n/2);
+        } else {
+            return power(i, n/2) * power(i, n/2) * i;
+        }
+    }
+}
