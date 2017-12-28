de15e4a9917dda4555a947aed36de7a5ab067a01
邢志壮 <1172113701@qq.com>
2017-12-28 23:22:52 +0800

    max area finished

--- src/leetcode/Q11ContainerWithMostWater.java
+++ src/leetcode/Q11ContainerWithMostWater.java
@@ -8,8 +8,30 @@ public class Q11ContainerWithMostWater {
 
     }
 
+//more simple loop inside
+    public static int maxAreaOptimal2(int[] height) {
+        if (height==null || height.length==0) { return 0; }
+        int max = 0;
+        int N = height.length;
+        int i = 0;
+        int j = N - 1;
+        while(i < j) {
+            int iMax = height[i], jMax = height[j];
+            max = Math.max(max, (j - i) * Math.min(height[i], height[j]));
+            if(height[i] < height[j]) {
+                while (i < j && height[i] < iMax) {
+                    ++i;
+                }
+            } else {
+                while (i < j && height[j] < jMax) {
+                    ++j;
+                }
+            }
+        }
+        return max;
+    }
 
-    public static int maxArea(int[] height) {
+    public static int maxAreaOptimal1(int[] height) {
         if (height==null || height.length==0) { return 0; }
         int max = 0;
         int N = height.length;
@@ -25,6 +47,24 @@ public class Q11ContainerWithMostWater {
                 int k = j - 1;
                 for (; k > i && height[k] <= height[j]; k--) {}
                 j = k;
+            }
+        }
+        return max;
+    }
+
+
+    public static int maxAreaCommon(int[] height) {
+        if (height==null || height.length==0) { return 0; }
+        int max = 0;
+        int N = height.length;
+        int i = 0;
+        int j = N - 1;
+        while(i < j) {
+            max = Math.max(max, (j - i) * Math.min(height[i], height[j]));
+            if(height[i] < height[j]) {
+                i++;
+            } else {
+                j--;
             }
         }
         return max;
