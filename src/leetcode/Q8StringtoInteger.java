fa91b68918ca40d84451db414c0e9e44c6d9461a
grande-xing <1172113701@qq.com>
2018-01-09 14:44:11 +0800

    q8 finished

--- src/leetcode/Q8StringtoInteger.java
+++ src/leetcode/Q8StringtoInteger.java
@@ -2,7 +2,44 @@ package leetcode;
 
 public class Q8StringtoInteger {
     public static void main(String[] args) {
+        String s = " -1010023630o4";
+        System.out.println(myAtoi1(s));
+    }
 
+
+    public static int myAtoi1(String str) {
+        if (str == null || str.length() == 0) return 0;
+        char[] cs = str.toCharArray();
+//         for white space in the front
+        int i = 0;
+        long sgn = 1L;
+        long value = 0L;
+        while (cs[i] == ' ') i++;
+        if (cs[i] == '+') {
+            sgn = 1L;
+            i++;
+        } else if (cs[i] == '-') {
+            sgn = -1L;
+            i++;
+        }
+        int j = 0;
+        while (i < cs.length) {
+            if (cs[i] > 58 || cs[i] < 47) break;
+            if (j > 11){
+                if (sgn>0) return Integer.MAX_VALUE;
+                else return Integer.MIN_VALUE;
+            }
+            value = value * 10L + (long)(cs[i] - 48);
+            i++;
+            j++;
+//            System.out.println(value);
+        }
+//        System.out.println(value);
+        value = value * sgn;
+        System.out.println(value);
+        if (value > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+        if (value < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+        return (int) value;
     }
 
 
