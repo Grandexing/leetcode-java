3ed6fa8ed9fcc71b60481e4bd830d410f3c1d8c0
grande-xing <1172113701@qq.com>
2018-01-12 15:26:56 +0800

    Q92 not finished

--- src/leetcode/Q92ReverseLinkedListII.java
+++ src/leetcode/Q92ReverseLinkedListII.java
@@ -9,6 +9,20 @@ public class Q92ReverseLinkedListII {
 
 
     public static ListNode reverseBetween(ListNode head, int m, int n) {
+        if((n - m) == 0) return head;
+        ListNode p, q, r;
+        int k = n - m;
+        p = head;
+        m--;
+        while (m > 0) {
+            p = p.next;
+            m--;
+        }
 
+        while (k > 0) {
+            q = p.next;
+            p.next = q.next;
+            q.next = p;
+        }
     }
 }
