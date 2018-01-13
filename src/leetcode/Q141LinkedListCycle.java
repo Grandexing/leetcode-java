6977211da735873e0e8dc041f8afe66a4bd37570
grande-xing <1172113701@qq.com>
2018-01-13 15:31:00 +0800

    Q141

---
+++ src/leetcode/Q141LinkedListCycle.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+import leetcode.classes.ListNode;
+
+public class Q141LinkedListCycle {
+    public static boolean hasCycle(ListNode head) {
+        if(head==null) return false;
+        ListNode walker, runner;
+        walker = head;
+        runner = head;
+        while(runner.next != null
+                && runner.next.next != null) {
+            walker = walker.next;
+            runner = runner.next.next;
+            if (walker == runner) return true;
+        }
+        return false;
+    }
+}
