5225170d192e3cebb4a5a52751e01bac3a316af8
grande-xing <1172113701@qq.com>
2018-01-16 10:57:27 +0800

    sword-q3

---
+++ src/sword2offer/Q3.java
@@ -0,0 +1,25 @@
+package sword2offer;
+
+public class Q3 {
+    
+    static boolean find(int target, int [][] array) {
+        boolean found = false;
+        if(array == null || array.length == 0 || array[0].length == 0) return found;
+        int rows = array.length;
+        int columns = array[0].length;
+        int row = 0;
+        int column = columns- 1;
+        while (row < rows && column >= 0) {
+            if (array[row][column] == target) {
+                found = true;
+                break;
+            }
+            if (array[row][column] > target) {
+                column--;
+            } else {
+                row++;
+            }
+        }
+        return found;
+    }
+}
