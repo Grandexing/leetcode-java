a0b7d3ff4ddd94760725cad77ada09b8ec2b7dc6
grande-xing <1172113701@qq.com>
2018-01-23 15:17:21 +0800

    sword Q19

--- src/sword2offer/Q19.java
+++ src/sword2offer/Q19.java
@@ -4,6 +4,32 @@ public class Q19 {
     public static void main(String[] args) {
         System.out.println(match("".toCharArray(), ".*".toCharArray()));
     }
+// up-bottom
+    public boolean match2(char[] str, char[] pattern)
+    {
+        if(str == null || pattern == null) return false;
+        Boolean[][] dp = new Boolean[str.length + 1][pattern.length + 1];
+        return isMatch(str, pattern, 0, 0, dp);
+    }
+
+
+    boolean isMatch(char[] s, char[] p, int sIndex, int pIndex, Boolean[][] dp) {
+        if (dp[sIndex][pIndex] != null) return dp[sIndex][pIndex];
+        boolean ans;
+        if (pIndex == p.length) {
+            ans = sIndex == s.length;
+        } else {
+            boolean firstMatch = sIndex < s.length && (p[pIndex] == s[sIndex] || p[pIndex] == '.');
+            if (pIndex + 1 < p.length && p[pIndex + 1] == '*') {
+                ans = isMatch(s, p, sIndex, pIndex + 2, dp) || (firstMatch && isMatch(s, p, sIndex + 1, pIndex, dp));
+            } else {
+                ans = firstMatch && isMatch(s, p, sIndex + 1, pIndex + 1, dp);
+            }
+        }
+        dp[sIndex][pIndex] = ans;
+        return ans;
+    }
+
 
 //动态规划（bottom up）
     public boolean match1(char[] str, char[] pattern)
