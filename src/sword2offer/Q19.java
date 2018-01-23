5dd14f6b0ae5ab99339ddce869421f59b37e4e65
grande-xing <1172113701@qq.com>
2018-01-23 14:24:09 +0800

    sword Q19

--- src/sword2offer/Q19.java
+++ src/sword2offer/Q19.java
@@ -5,6 +5,37 @@ public class Q19 {
         System.out.println(match("".toCharArray(), ".*".toCharArray()));
     }
 
+//动态规划（bottom up）
+    public boolean match1(char[] str, char[] pattern)
+    {
+        if (str == null || pattern == null) return false;
+        boolean[][] dp = new boolean[str.length + 1][pattern.length + 1];
+        dp[0][0] = true;
+        for (int i = 0; i < pattern.length; i++) {
+            if (pattern[i] == '*' && dp[0][i - 1]) {
+                dp[0][i + 1] = true;
+            }
+        }
+        for (int i = 0; i < str.length; i++) {
+            for (int j = 0; j < pattern.length; j++) {
+                if (pattern[j] == '.') {
+                    dp[i + 1][j + 1] = dp[i][j];
+                }
+                if (pattern[j] == str[i]) {
+                    dp[i + 1][j + 1] = dp[i][j];
+                }
+                if (pattern[j] == '*') {
+                    if (pattern[j - 1] != str[i] && pattern[j - 1] != '.') {
+                        dp[i + 1][j + 1] = dp[i + 1][j - 1];
+                    } else {
+                        dp[i + 1][j + 1] = dp[i + 1][j] || dp[i][j + 1] || dp[i + 1][j - 1];
+                    }
+                }
+            }
+        }
+        return dp[str.length][pattern.length];
+    }
+
 
     public static boolean match(char[] str, char[] pattern)
     {
@@ -18,14 +49,15 @@ public class Q19 {
         if (strIndex != str.length && patternIndex == pattern.length) return false;
         if (patternIndex < pattern.length - 1 && pattern[patternIndex + 1] == '*') {
             if ((strIndex != str.length && pattern[patternIndex] == str[strIndex]) || (pattern[patternIndex] == '.' && strIndex < str.length)) {
-                return match(str, pattern, strIndex + 1, patternIndex + 2)
-                        || match(str, pattern, strIndex + 1, patternIndex)
-                        || match(str, pattern, strIndex, patternIndex + 2);
+                return match(str, pattern, strIndex, patternIndex + 2)
+//                        此情况包含在另两种情况中，注释掉也可以通过
+//                        || match(str, pattern, strIndex + 1, patternIndex + 2)
+                        || match(str, pattern, strIndex + 1, patternIndex);
             } else {
                 return match(str, pattern, strIndex, patternIndex + 2);
             }
         }
-        if (pattern[patternIndex] == str[strIndex] || (pattern[patternIndex] == '.' && strIndex < str.length)) {
+        if ((strIndex != str.length && pattern[patternIndex] == str[strIndex]) || (pattern[patternIndex] == '.' && strIndex < str.length)) {
             return match(str, pattern, strIndex + 1, patternIndex + 1);
         }
         return false;
