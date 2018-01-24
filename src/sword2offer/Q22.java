6eab11b54a1b5087858c94b20728d0abffa9714c
grande-xing <1172113701@qq.com>
2018-01-24 10:52:20 +0800

    sword Q22

--- src/sword2offer/Q22.java
+++ src/sword2offer/Q22.java
@@ -4,10 +4,12 @@ import classes.ListNode;
 
 public class Q22 {
     public ListNode FindKthToTail(ListNode head, int k) {
+//        k从1开始，k=1代表倒数第一个节点
         if (head == null || k <= 0) return null;
         ListNode pNode1, pNode2;
         pNode1 = head;
         for (int i = 0; i < k - 1; i++) {
+//            k-2 的下一个节点不能是null，保证链表至少有k个节点
             if (pNode1.next != null) {
                 pNode1 = pNode1.next;
             } else {
