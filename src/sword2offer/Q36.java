476a8ef2dfe6e29f22c47f7a4f569e7a1d5d2310
grande-xing <1172113701@qq.com>
2018-01-26 18:01:48 +0800

    sword Q36 not finished

--- src/sword2offer/Q36.java
+++ src/sword2offer/Q36.java
@@ -1,4 +1,23 @@
 package sword2offer;
 
+import classes.TreeNode;
+
 public class Q36 {
+    public TreeNode Convert(TreeNode pRootOfTree) {
+        if (pRootOfTree == null) return null;
+        TreeNode lastNodeInList = null;
+        lastNodeInList = convert(pRootOfTree, lastNodeInList);
+        TreeNode headOfList = lastNodeInList;
+        while (headOfList.left != null) {
+            headOfList = headOfList.left;
+        }
+        return headOfList;
+    }
+
+
+    public TreeNode convert(TreeNode pNode, TreeNode lastNodeInList) {
+        if (pNode == null) return lastNodeInList;
+        TreeNode pCurrent = pNode;
+
+    }
 }
