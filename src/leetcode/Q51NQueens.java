e31f10ab58d80504f7d4764c3c496e722c8b72b6
grande-xing <1172113701@qq.com>
2018-01-29 15:34:25 +0800

    Q51

--- src/leetcode/Q51NQueens.java
+++ src/leetcode/Q51NQueens.java
@@ -7,66 +7,91 @@ import java.util.List;
 public class Q51NQueens {
     public static void main(String[] args) {
         Q51NQueens q51NQueens = new Q51NQueens();
-        List<List<String>> result = q51NQueens.solveNQueens(5);
+        List<List<String>> result = q51NQueens.solveNQueens_1(4);
         for (List<String> l : result) {
-            for (String s : l) {
+            for (String s:l) {
                 System.out.println(s);
             }
+            System.out.println();
         }
     }
 
 
+    public List<List<String>> solveNQueens_1(int n) {
+        List<List<String>> lists = new ArrayList<List<String>>();
+//        List<String> list = new ArrayList<String>();
+        int[] colIndex = new int[n];
+        solveNQueens(n, 0, lists, colIndex);
+        return lists;
+    }
+
+    public void solveNQueens(int n, int row, List<List<String>> lists, int[] colIndex){
+//        char[] rowArray = new char[n];
+//        for(int i = 0; i < n; i++){
+//            rowArray[i] = '.';
+//        }
+        if(row == n) {
+//            lists.add(new ArrayList<String>(list));
+            lists.add(construct(colIndex));
+            return;
+        }
+        for(int i = 0; i < n; i++) {
+            if(checkValid(row, i, colIndex)){
+                colIndex[row] = i;
+//                rowArray[i] = 'Q';
+//                list.add(new String(rowArray));
+                solveNQueens(n, row + 1, lists, colIndex);
+//                list.remove(list.size() - 1);
+//                rowArray[i] = '.';
+            }
+        }
+    }
+
+    private List<String> construct(int[] colIndex) {
+        List<String> one = new ArrayList<>();
+        for(int i=0;i<colIndex.length;i++){
+            StringBuilder s=new StringBuilder();
+            for(int j=0;j<colIndex.length;j++){
+                s.append(".");
+            }
+            s.setCharAt(colIndex[i],'Q');
+            one.add(s.toString());
+        }
+        return one;
+    }
+
+    public boolean checkValid(int row, int col, int[] colIndex){
+        for(int i = 0; i < row; i++){
+            if(col == colIndex[i]){
+                return false;
+            }
+
+            if(row - i == Math.abs(col - colIndex[i])){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
     public List<List<String>> solveNQueens(int n) {
-        //if (n <= 3) return new ArrayList<>();
         int[] cols = new int[n];
         for(int i = 0; i < n; i++) {
             cols[i] = i;
         }
-        // List<int[]> allPossible = new ArrayList<>();
-        // permitation(cols, allPossible, 0);
-//        for (int[] is: allPossible ) {
-//            for (int i : is) {
-//                System.out.print(i+",");
-//            }
-//            System.out.println();
-//        }
         List<int[]> allResult = new ArrayList<>();
         permitation(cols, allResult, 0);
-        // for (int[] possible : allPossible) {
-        //     boolean isPossible = true;
-        //     List<String> oneResult = new ArrayList<>();
-        //     for (int i = 0; i < n - 1 && isPossible; i++) {
-        //         for (int j = i + 1; j < n && isPossible; j++) {
-        //             if (Math.abs(j - i) == Math.abs(possible[j] - possible[i])) {
-        //                 isPossible = false;
-        //                 break;
-        //             }
-        //         }
-        //     }
-        //     if (isPossible) {
-        //         allResult.add(possible);
-        //     }
-        // }
-//        for (int[] is: allResult) {
-//            for (int i : is) {
-//                System.out.print(i+",");
-//            }
-//            System.out.println();
-//        }
         List<List<String>> result = new ArrayList<>();
+        char[] initial = new char[n];
+        for (int i = 0; i < n; i++) {
+            initial[i] = '.';
+        }
         for (int[] r : allResult) {
             List<String> one = new ArrayList<>();
             for (int i = 0; i < n; i++) {
-                StringBuffer sb = new StringBuffer();
-                for (int j = 0; j < n; j++) {
-                    if (r[i] == j) {
-                        sb.append("Q");
-                    } else {
-                        sb.append(".");
-                    }
-                }
-                one.add(sb.toString());
-//                System.out.println(sb.toString());
+                initial[r[i]] = 'Q';
+                one.add(new String(initial));
+                initial[r[i]] = '.';
             }
             result.add(one);
         }
