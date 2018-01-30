9151128dd5a3190123ec50d6fdfd6c39d13ef103
grande-xing <1172113701@qq.com>
2018-01-30 15:46:36 +0800

    MergeSort

--- src/aljs4j/sort/MergeSortBU.java
+++ src/aljs4j/sort/MergeSortBU.java
@@ -1,62 +1,33 @@
 package aljs4j.sort;
 
-import static aljs4j.sort.Sort.less;
-
-/**
- * Created by grande on 2018/1/29.
- */
 public class MergeSortBU {
-    //自顶向下的归并排序
+//    自底向上归并
     static int[] aux;
 
 
     public static void sort(int[] a) {
-        aux = new int[a.length];
-        sort(a, 0, a.length - 1);
-    }
-
-
-    static void sort(int[] a, int lo, int hi) {
-        if (lo >= hi) return;
-        int mid = lo + (hi - lo) / 2;
-        //排序左半边
-        sort(a, lo, mid);
-        //排序右半边
-        sort(a, mid + 1, hi);
-        merge(a, lo, mid, hi);
+        int N = a.length;
+        aux = new int[N];
+        for(int sz = 1; sz < N; sz = sz + sz) {
+            for (int lo = 0; lo < N - sz; lo += sz + sz) {
+                merge(a, lo, lo + sz - 1, Math.min(lo + sz + sz - 1, N - 1));
+            }
+        }
     }
 
 
     static void merge(int[] a, int lo, int mid, int hi) {
-        //将数据复制到辅助数组中
+        if (lo >= hi) return;
         for (int k = lo; k <= hi; k++) {
             aux[k] = a[k];
         }
-        //得到左右两个数组的起点
         int i = lo;
         int j = mid + 1;
-        //将左右两个有序数组归并为一个数组
         for (int k = lo; k <= hi; k++) {
-            //左边数组耗尽，这一步是否有必要？
-            if (i > mid) {
-                a[k] = aux[j];
-                j++;
-            }
-            //右边数组耗尽
-            else if (j > hi) {
-                a[k] = aux[i];
-                i++;
-            }
-            //右边小于左边，用右边
-            else if (aux[j] < aux[i]) {
-                a[k] = aux[j];
-                j++;
-            }
-            //左边小于等于右边，用左边
-            else {
-                a[k] = aux[i];
-                i++;
-            }
+            if (i > mid) a[k] = aux[j++];
+            else if (j > hi) a[k] = aux[i++];
+            else if (aux[j] < aux[i]) a[k] = aux[j++];
+            else a[k] = aux[i++];
         }
     }
 }
--- src/aljs4j/sort/MergeSortUB.java
+++ src/aljs4j/sort/MergeSortUB.java
@@ -1,32 +1,62 @@
 package aljs4j.sort;
 
+import static aljs4j.sort.Sort.less;
+
+/**
+ * Created by grande on 2018/1/29.
+ */
 public class MergeSortUB {
+    //自顶向下的归并排序
     static int[] aux;
 
 
     public static void sort(int[] a) {
-        int N = a.length;
-        aux = new int[N];
-        for(int sz = 1; sz < N; sz = sz + sz) {
-            for (int lo = 0; lo < N - sz; lo += sz + sz) {
-                merge(a, lo, lo + sz - 1, Math.min(lo + sz + sz - 1, N - 1));
-            }
-        }
+        aux = new int[a.length];
+        sort(a, 0, a.length - 1);
+    }
+
+
+    static void sort(int[] a, int lo, int hi) {
+        if (lo >= hi) return;
+        int mid = lo + (hi - lo) / 2;
+        //排序左半边
+        sort(a, lo, mid);
+        //排序右半边
+        sort(a, mid + 1, hi);
+        merge(a, lo, mid, hi);
     }
 
 
     static void merge(int[] a, int lo, int mid, int hi) {
-        if (lo >= hi) return;
+        //将数据复制到辅助数组中
         for (int k = lo; k <= hi; k++) {
             aux[k] = a[k];
         }
+        //得到左右两个数组的起点
         int i = lo;
         int j = mid + 1;
+        //将左右两个有序数组归并为一个数组
         for (int k = lo; k <= hi; k++) {
-            if (i > mid) a[k] = aux[j++];
-            else if (j > hi) a[k] = aux[i++];
-            else if (aux[j] < aux[i]) a[k] = aux[j++];
-            else a[k] = aux[i++];
+            //左边数组耗尽，这一步是否有必要？
+            if (i > mid) {
+                a[k] = aux[j];
+                j++;
+            }
+            //右边数组耗尽
+            else if (j > hi) {
+                a[k] = aux[i];
+                i++;
+            }
+            //右边小于左边，用右边
+            else if (aux[j] < aux[i]) {
+                a[k] = aux[j];
+                j++;
+            }
+            //左边小于等于右边，用左边
+            else {
+                a[k] = aux[i];
+                i++;
+            }
         }
     }
 }
