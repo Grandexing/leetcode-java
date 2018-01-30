7744bde9536345a1a76f6513f632379e17bc8150
grande-xing <1172113701@qq.com>
2018-01-30 14:53:52 +0800

    MergeSort

---
+++ src/aljs4j/sort/MergeSortUB.java
@@ -0,0 +1,32 @@
+package aljs4j.sort;
+
+public class MergeSortUB {
+    static int[] aux;
+
+
+    public static void sort(int[] a) {
+        int N = a.length;
+        aux = new int[N];
+        for(int sz = 1; sz < N; sz = sz + sz) {
+            for (int lo = 0; lo < N - sz; lo += sz + sz) {
+                merge(a, lo, lo + sz - 1, Math.min(lo + sz + sz - 1, N - 1));
+            }
+        }
+    }
+
+
+    static void merge(int[] a, int lo, int mid, int hi) {
+        if (lo >= hi) return;
+        for (int k = lo; k <= hi; k++) {
+            aux[k] = a[k];
+        }
+        int i = lo;
+        int j = mid + 1;
+        for (int k = lo; k <= hi; k++) {
+            if (i > mid) a[k] = aux[j++];
+            else if (j > hi) a[k] = aux[i++];
+            else if (aux[j] < aux[i]) a[k] = aux[j++];
+            else a[k] = aux[i++];
+        }
+    }
+}
