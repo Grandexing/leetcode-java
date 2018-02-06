10c9953fce5f02b97214e414d08e95f1d001a5db
grande-xing <1172113701@qq.com>
2018-02-06 21:06:12 +0800

    sword Q57Part2

--- src/sword2offer/Q57Part2.java
+++ src/sword2offer/Q57Part2.java
@@ -3,6 +3,12 @@ package sword2offer;
 import java.util.ArrayList;
 
 public class Q57Part2 {
+// 面试题57（二）：为s的连续正数序列
+// 题目：输入一个正数s，打印出所有和为s的连续正数序列（至少含有两个数）。
+// 例如输入15，由于1+2+3+4+5=4+5+6=7+8=15，所以结果打印出3个连续序列1～5、
+// 4～6和7～8。
+
+
     public ArrayList<ArrayList<Integer>> FindContinuousSequence(int sum) {
         ArrayList<ArrayList<Integer>> result = new ArrayList<>();
         if (sum < 3) return result;
