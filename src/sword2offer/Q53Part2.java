7f6c46cc80f5a2f9c7800347bfdab1e7e8a0a8a8
grande-xing <1172113701@qq.com>
2018-02-06 10:57:31 +0800

    sword Q53Part2

--- src/sword2offer/Q53Part2.java
+++ src/sword2offer/Q53Part2.java
@@ -1,6 +1,13 @@
 package sword2offer;
 
 public class Q53Part2 {
+
+// 面试题53（二）：0到n-1中缺失的数字
+// 题目：一个长度为n-1的递增排序数组中的所有数字都是唯一的，并且每个数字
+// 都在范围0到n-1之内。在范围0到n-1的n个数字中有且只有一个数字不在该数组
+// 中，请找出这个数字。
+
+
     public static void main(String[] args) {
         Q53Part2 q53Part2 = new Q53Part2();
         q53Part2.test();
@@ -15,20 +22,23 @@ public class Q53Part2 {
             while (start <= end) {
                 int mid = start + (end - start) / 2;
                 if (numbers[mid] != mid) {
+//                    如果值和下标不等
                     if ((mid - 1 > 0 && numbers[mid - 1] == mid - 1) || mid == 0) {
+//                        这个值的是第一个值和下标不等的位置
                         return mid;
                     } else {
+//                        第一个值和下标不等的值在当前mid的左边
                         end = mid - 1;
                     }
                 } else {
+//                    第一个值和下标不等的值在mid的右边
                     start = mid + 1;
                 }
             }
-            if (start == end) {
+            if (start >= end) {
                 result = numbers.length;
             }
         }
-        System.out.println(result);
         return result;
     }
 
