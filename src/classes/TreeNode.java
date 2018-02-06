4092a2be791ead444568e4b10cfc1e6365283de2
grande-xing <1172113701@qq.com>
2018-02-06 16:24:23 +0800

    sword Q55

--- src/classes/TreeNode.java
+++ src/classes/TreeNode.java
@@ -12,4 +12,15 @@ public class TreeNode {
     public TreeNode(int x) {
         val = x;
     }
+
+
+    public static TreeNode CreateTreeNode(int i) {
+        return new TreeNode(i);
+    }
+
+    public static void ConnectTreeNodes(TreeNode root, TreeNode left, TreeNode right) {
+        if (root == null) return;
+        root.left = left;
+        root.right = right;
+    }
 }
---
+++ src/sword2offer/Q55Part1.java
@@ -0,0 +1,15 @@
+package sword2offer;
+
+import classes.TreeNode;
+
+public class Q55Part1 {
+// 面试题55（一）：二叉树的深度
+// 题目：输入一棵二叉树的根结点，求该树的深度。从根结点到叶结点依次经过的
+// 结点（含根、叶结点）形成树的一条路径，最长路径的长度为树的深度。
+    public int TreeDepth(TreeNode root) {
+        if (root == null) return 0;
+        int left = TreeDepth(root.left);
+        int right = TreeDepth(root.right);
+        return left > right ? left + 1 : right + 1;
+    }
+}
