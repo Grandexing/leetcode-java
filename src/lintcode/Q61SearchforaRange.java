f3376256fb545f25bb1453a5416ad15b02b42001
grande-xing <1172113701@qq.com>
2018-02-17 10:54:31 +0800

    lintcode Q61

---
+++ src/lintcode/Q61SearchforaRange.java
@@ -0,0 +1,48 @@
+package lintcode;
+
+public class Q61SearchforaRange {
+
+
+    public int[] searchRange(int[] A, int target) {
+        // write your code here
+        int[] result = {-1, -1};
+        if (A == null || A.length == 0) {
+            return result;
+        }
+        // search for left
+        int start = 0;
+        int end = A.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (A[mid] >= target) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+        if (A[start] == target) {
+            result[0] = start;
+        } else if (A[end] == target){
+            result[0] = end;
+        } else {
+            return result;
+        }
+        // search for right
+//        not need to set start
+        end = A.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (A[mid] <= target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        if (A[end] == target) {
+            result[1] = end;
+        } else if (A[start] == target) {
+            result[1] = start;
+        }
+        return result;
+    }
+}
