c466434f42d8543e647356c7ed00c65a715f70bb
grande-xing <1172113701@qq.com>
2018-02-20 11:58:45 +0800

    lintcode Q378, Q435

---
+++ src/lintcode/Q453FlattenBinaryTreetoLinkedList.java
@@ -0,0 +1,53 @@
+package lintcode;
+
+import classes.TreeNode;
+
+public class Q453FlattenBinaryTreetoLinkedList {
+//    http://www.lintcode.com/en/problem/flatten-binary-tree-to-linked-list/
+
+    private TreeNode lastNode = null;
+    public void flatten(TreeNode root) {
+        // write your code here
+        if (root == null) {
+            return;
+        }
+        if (lastNode != null) {
+            lastNode.left = null;
+            lastNode.right = root;
+        }
+        lastNode = root;
+        TreeNode right = root.right;
+        flatten(root.left);
+        flatten(right);
+    }
+
+
+    public void flatten1(TreeNode root) {
+        // write your code here
+        if (root == null) {
+            return;
+        }
+        flat(root);
+    }
+
+
+    private TreeNode flat(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode leftLast = flat(root.left);
+        TreeNode rightLast = flat(root.right);
+        if (leftLast != null) {
+            leftLast.right = root.right;
+            root.right = root.left;
+            root.left = null;
+        }
+        if (rightLast != null) {
+            return rightLast;
+        }
+        if (leftLast != null) {
+            return leftLast;
+        }
+        return root;
+    }
+}
