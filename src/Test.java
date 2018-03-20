6160e29e51a1502898662959a5c1098f44d5a42b
grande-xing <1172113701@qq.com>
2018-03-20 21:36:20 +0800

    lintcode Q53

--- src/Test.java
+++ src/Test.java
@@ -1,12 +1,34 @@
+import java.util.Arrays;
 import java.util.Stack;
 
 public class Test {
     public static void main(String[] args) {
 //        String a = new String("123");
 //        String b = new String("123");
-        String a = "123";
-        String b = "123";
-        System.out.println(a == b);
-        System.out.println(a.equals(b));
+        char[] cs = "1   2".toCharArray();
+        int end = removeExtraSpace(cs);
+        System.out.println(end);
+        System.out.println(new String(Arrays.copyOf(cs, 3)));
+    }
+
+
+    private static int removeExtraSpace(char[] cs) {
+        int end = 0;
+        int index = 0;
+        while (index < cs.length) {
+            if (cs[index] == ' ' && index > 0 && cs[index - 1] == ' ') {
+                index++;
+            } else {
+//                end++;
+                if (index != end) {
+                    System.out.println(end);
+                    System.out.println(index);
+                    cs[end] = cs[index];
+                }
+                index++;
+                end++;
+            }
+        }
+        return end;
     }
 }
--- src/lintcode/Q53ReverseWordsinaString.java
+++ src/lintcode/Q53ReverseWordsinaString.java
@@ -1,5 +1,7 @@
 package lintcode;
 
+import java.util.Arrays;
+
 public class Q53ReverseWordsinaString {
 //    http://www.lintcode.com/en/problem/reverse-words-in-a-string/
 
@@ -19,5 +21,66 @@ public class Q53ReverseWordsinaString {
             }
         }
         return sb.toString();
+    }
+
+
+    public String reverseWords1(String s) {
+        // write your code here
+//        这种方法太复杂了...
+        if (s == null || s.isEmpty()) {
+            return s;
+        }
+        char[] cs = s.trim().toCharArray();
+        int end = removeExtraSpace(cs);
+        reverse(cs, 0, end - 1);
+        int start = 0;
+        for (int i = 1; i < end; i++) {
+            if (cs[i] == ' ') {
+                reverse(cs, start, i - 1);
+                start = i + 1;
+            } else if (i == end - 1){
+                reverse(cs, start, i);
+            }
+        }
+        return new String(Arrays.copyOf(cs, end));
+    }
+
+
+    private int removeExtraSpace(char[] cs) {
+        int end = 0;
+        int index = 0;
+        while (index < cs.length) {
+            if (cs[index] == ' ' && index > 0 && cs[index - 1] == ' ') {
+                index++;
+            } else {
+                if (index != end) {
+                    cs[end] = cs[index];
+                }
+                index++;
+                end++;
+            }
+        }
+        return end;
+    }
+
+
+    private void reverse(char[] cs, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+        int left = start;
+        int right = end;
+        while(left < right) {
+            swap(cs, left, right);
+            left++;
+            right--;
+        }
+    }
+
+
+    private void swap(char[] cs, int i, int j) {
+        char tmp = cs[i];
+        cs[i] = cs[j];
+        cs[j] =tmp;
     }
 }
